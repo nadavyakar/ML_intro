efa070289000e5eb504527ebc0d39ea4c6605f93
nadav yakar <nadavyakar@gmail.com>
2018-04-14 21:31:27 +0300

    ex2 finished except plot, untested, finish reading remarks on piazza and whatsup

--- ex2/ex2.py
+++ ex2/ex2.py
@@ -5,6 +5,7 @@ from math import exp, sqrt, pi, pow
 nclass=3
 nsamples_per_class=100
 epocs=1
+learning_rate=.1
 
 def f(x,y):
     return exp(-pow(x-2*y,2)/2)/sqrt(2*pi)
@@ -20,19 +21,20 @@ def generate_training_set():
     return training_set
 def generate_dummy_training():
     return [(1,2),(2,4),(3,6)]
-def derivative(W,x,i):
-    return sum([ ???? ])
-def upadte(W,x):
-
-    for w in W.shape()
-    np.dot()
+def upadte(W,x,y):
+    derivative = [.0]*nclass
+    derivative_denominator=sum([exp(k) for k in np.asarray(np.dot(W, x)).reshape(-1)])
+    for i in range(nclass):
+        derivative_nominator=exp(np.dot(W[i, :], x))
+        derivative[i] = ((derivative_nominator/derivative_denominator)-1)*x if i==y \
+            else (derivative_nominator/derivative_denominator)*x
+    for i in range(nclass):
+        W[i] -= learning_rate*derivative[i]
 def train_logistic_regression(train):
     W=np.matrix([[0]*2]*nclass)
     for e in range(epocs):
         for x,y in train:
-            x_1=np.array([x,1])
-            update(W,x_1)
-            todo
+            upadte(W, np.array([x,1]), y)
     def p(y, x):
         return exp(np.dot(W[y,:],x)) / sum([ exp(k) for k in np.asarray(np.dot(W, x)).reshape(-1) ])
     return p
