2a3fbe917dc57bdc697e3673e818b971e5c66d1a
nadav yakar <nadavyakar@gmail.com>
2018-05-11 17:49:12 +0300

    ex3 unfinished

--- ex3/ex_3.py
+++ ex3/ex_3.py
@@ -7,48 +7,65 @@ learning_rate=0.01
 hidden_layer_size=pic_size
 batch_size=1
 validation_ratio=.2
+Y=dict([(y,[ 1 if i==y else 0 for i in range(nclasses)]) for y in range(nclasses) ])
 
 def init_model():
     W1=np.matrix([[0.5] * pic_size+1] * hidden_layer_size)
     W2=np.matrix([[0.5] * hidden_layer_size+1] * nclasses)
     return (W1,W2)
-class Activation:
-    def __init__(self, c=1):
-        self.c = c
-    def __call__(self, val):
-        try:
-            x = 1.0 / (1.0 + exp(-self.c*val))
-        except Exception:
-            return 0 if val<0 else 1
-        return x
+class Sigmoid:
+    def __call__(self, IN_VEC):
+        X=[]
+        for val in IN_VEC:
+            try:
+                X.append(1.0 / (1.0 + exp(-self.c*val)))
+            except Exception:
+                X.append(0 if val<0 else 1)
+        return X
     def derivative(self, output):
         return (1.0 - output) * output
-activation=Activation()
+class Softmax:
+    def __call__(self, V):
+        denominator = sum([exp(v) for v in V])
+        return [exp(v) / denominator for v in V ]
+    def derivative(self, V, Y):
+        return V-Y
+
+activation=[Sigmoid(), Softmax()]
 def split_to_valid(train_x,train_y):
     data_set=zip(train_x, train_y)
     shuffle(data_set)
     train_size=len(data_set)-int(validation_ratio*len(data_set))
     return data_set[:train_size],data_set[train_size:]
+def bk_prop(model,X,Y,W):
+    V=fw_prop(model,X)
+    #update weights to last layer
+    err = activation[1].derivative(V[1],Y)
+    derivative = np.dot(err, V[0])
+    W[1]-=learning_rate*derivative
+    #update weights to hidden layer
+    err=np.dot(W[1],err)
+    g=np.dot(activation[0].derivative(V[0]),err)
+    derivative = np.dot(g, X+[1])
+    W[0]-=learning_rate*derivative
+
 def train(train_x,train_y):
-    #todo split to validation set
     train,valid=split_to_valid(train_x,train_y)
     model=init_model()
     for e in range(epocs):
         #shuffle(train)
         for X,y in train:
-            ...
-    return model
-def fw_prop(model,X):
+            bk_prop(model,X,Y[y],W)
+    return W
+def fw_prop(W,X):
+    V={}
     #in layer to hidden
-    W1=model[0]
-    l1_in_vector=np.dot(W1,X+[1])
-    l1_out_vector=[activation(y) for y in l1_in_vector]
+    l1_in_vector=np.dot(W[0],X+[1])
+    V[0]=activation[0](l1_in_vector)
     #hidden to out layer
-    W2=model[1]
-    l2_in_vector=np.dot(W2,l1_out_vector+[1])
-    #todo should the activation be softmax and backprop as in ex2 instead of reg activation?
-    l2_out_vector=[activation(y) for y in l2_in_vector]
-    return l2_out_vector
+    l2_in_vector=np.dot(W[1],V[0]+[1])
+    V[1]=activation[1](l2_in_vector)
+    return V
 def test(model,test_x):
     '''
     :param model: a learned weight matrices. the 1st layer W matrix width=|X|=pic_size & high=hidden_layer_size
@@ -58,7 +75,7 @@ def test(model,test_x):
     '''
     out=[]
     for X in test_x:
-        out.append(fw_prop(model,X))
+        out.append(fw_prop(model,X)[-1])
     return out
 # train_x = np.loadtxt("train_x")
 # train_y = np.loadtxt("train_y")
@@ -67,8 +84,8 @@ train_x = np.loadtxt("train_x_top_10")
 train_y = np.loadtxt("train_y_top_10")
 test_x = np.loadtxt("test_x_top_10")
 
-model=train([[pixel/255 for pixel in pic] for pic in train_x],train_y)
-out=test(model,test_x)
+W=train([[pixel/255 for pixel in pic] for pic in train_x],train_y)
+out=test(W,test_x)
 with open('test.pred','w') as f:
     for l in out:
         write(l)
