b2e6b46610f0a8d68ba96fc07406df211090a074
nadav yakar <nadavyakar@gmail.com>
2018-05-11 20:27:28 +0300

    ex3 bk prop should be rewritten

--- ex3/ex_3.py
+++ ex3/ex_3.py
@@ -1,4 +1,6 @@
 import numpy as np
+from random import shuffle
+from math import log, exp
 
 pic_size=784
 nclasses=10
@@ -10,35 +12,45 @@ validation_ratio=.2
 Y=dict([(y,[ 1 if i==y else 0 for i in range(nclasses)]) for y in range(nclasses) ])
 
 def init_model():
-    W1=np.matrix([[0.5] * pic_size+1] * hidden_layer_size)
-    W2=np.matrix([[0.5] * hidden_layer_size+1] * nclasses)
+    W1=np.matrix([[0.5] * (pic_size+1)] * hidden_layer_size)
+    W2=np.matrix([[0.5] * (hidden_layer_size+1) ] * nclasses)
     return (W1,W2)
 class Sigmoid:
     def __call__(self, IN_VEC):
         X=[]
         for val in IN_VEC:
             try:
-                X.append(1.0 / (1.0 + exp(-self.c*val)))
+                X.append(1.0 / (1.0 + exp(-val)))
             except Exception:
                 X.append(0 if val<0 else 1)
         return X
-    def derivative(self, output):
-        return (1.0 - output) * output
+    def derivative(self, V):
+        return (1.0 - V) * V
 class Softmax:
-    def __call__(self, V):
-        denominator = sum([exp(v) for v in V])
-        return [exp(v) / denominator for v in V ]
+    def __call__(self, IN_VEC):
+        denominator = sum([exp(v) for v in IN_VEC])
+        return [exp(v) / denominator for v in IN_VEC ]
     def derivative(self, V, Y):
-        return V-Y
-
+        return [v-y for v,y in zip(V,Y)]
 activation=[Sigmoid(), Softmax()]
+def loss(V,y):
+    return -log(V[y])
 def split_to_valid(train_x,train_y):
     data_set=zip(train_x, train_y)
-    shuffle(data_set)
+    #shuffle(data_set)
     train_size=len(data_set)-int(validation_ratio*len(data_set))
     return data_set[:train_size],data_set[train_size:]
-def bk_prop(model,X,Y,W):
-    V=fw_prop(model,X)
+def fw_prop(W,X):
+    V={}
+    #in layer to hidden
+    l1_in_vector=np.dot(W[0],(X+[1]))
+    V[0]=activation[0](l1_in_vector.reshape(l1_in_vector.shape[1],-1))
+    #hidden to out layer
+    l2_in_vector=np.dot(W[1],(V[0]+[1]))
+    V[1]=activation[1](l2_in_vector.reshape(l2_in_vector.shape[1],-1))
+    return V
+def bk_prop(W,X,Y):
+    V=fw_prop(W,X)
     #update weights to last layer
     err = activation[1].derivative(V[1],Y)
     derivative = np.dot(err, V[0])
@@ -48,24 +60,24 @@ def bk_prop(model,X,Y,W):
     g=np.dot(activation[0].derivative(V[0]),err)
     derivative = np.dot(g, X+[1])
     W[0]-=learning_rate*derivative
-
+def validate(W,valid):
+    sum_loss= 0.0
+    correct=0.0
+    for X, y in valid:
+        V = fw_prop(W,X)
+        sum_loss += loss(V[-1],y)
+        if V[-1].index(max(V[-1])) == y:
+            correct += 1
+    return sum_loss/ len(valid),correct/ len(valid)
 def train(train_x,train_y):
     train,valid=split_to_valid(train_x,train_y)
-    model=init_model()
+    W=init_model()
     for e in range(epocs):
         #shuffle(train)
         for X,y in train:
-            bk_prop(model,X,Y[y],W)
+            bk_prop(W,X,Y[y])
+        print "epoc {} avg loss,accuricy {}".format(e,validate(W,valid))
     return W
-def fw_prop(W,X):
-    V={}
-    #in layer to hidden
-    l1_in_vector=np.dot(W[0],X+[1])
-    V[0]=activation[0](l1_in_vector)
-    #hidden to out layer
-    l2_in_vector=np.dot(W[1],V[0]+[1])
-    V[1]=activation[1](l2_in_vector)
-    return V
 def test(model,test_x):
     '''
     :param model: a learned weight matrices. the 1st layer W matrix width=|X|=pic_size & high=hidden_layer_size
@@ -83,6 +95,7 @@ def test(model,test_x):
 train_x = np.loadtxt("train_x_top_10")
 train_y = np.loadtxt("train_y_top_10")
 test_x = np.loadtxt("test_x_top_10")
+
 
 W=train([[pixel/255 for pixel in pic] for pic in train_x],train_y)
 out=test(W,test_x)
