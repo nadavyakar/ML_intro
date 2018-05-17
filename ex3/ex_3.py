704ab59edf706cb0a2c28c5ff247030cff7e04dd
nadav yakar <nadavyakar@gmail.com>
2018-05-17 17:37:10 +0300

    ex3 ready to test the real thing

--- ex3/ex_3.py
+++ ex3/ex_3.py
@@ -5,34 +5,11 @@ import pickle
 import sys
 import matplotlib.pyplot as plt
 import logging
+import time
 
-pic_size=784
-nclasses=10
-batch_size=1
-validation_ratio=.2
-epocs=[50,100,200,300]
-learning_rates=[0.05,1]
-architectures=[[pic_size,10,nclasses],[pic_size,100,nclasses],[pic_size,400,nclasses]]
-weight_init_boundries=[0.08,0.5]
-
-Y=dict([(y,[ 1 if i==y else 0 for i in range(nclasses)]) for y in range(nclasses) ])
-rnd.seed(1)
-
-# logger=logging.getLogger(__name__)
-logging.basicConfig(filename="/home/nadav/data/nn.log",level=logging.ERROR)
-
-def init_model(params):
-    layer_sizes, weight_init_boundry = params
-    return [ np.matrix([[rnd.uniform(-weight_init_boundry,weight_init_boundry)] * (layer_sizes[l]+1)] * layer_sizes[l+1]) for l in range(len(layer_sizes)-1) ]
 class ActivationSigmoid:
     def __call__(self, IN_VEC):
-        X=[]
-        for val in IN_VEC:
-            try:
-                X.append(1. / (1. + exp(-val)))
-            except Exception:
-                X.append(0. if val<0 else 1.)
-        return X
+        return 1. / (1. + np.exp(-IN_VEC))
     def derivative(self, out):
         return (1.0 - out) * out
 class ActivationSoftmax:
@@ -46,10 +23,94 @@ class ActivationInputIdentity:
         return IN_VEC
     def derivative(self, out):
         return np.array([.0,])
-activation=[ActivationInputIdentity(), ActivationSigmoid(), ActivationSoftmax()]
+
+#
+pic_size=784
+nclasses=10
+# remove:
+data = open(r"/home/nadav/data/data.txt", "rb").read()
+train_x,train_y,test_x = pickle.loads(data)
+
+#train_x = np.loadtxt("train_x")
+#train_y = np.loadtxt("train_y")
+#test_x = np.loadtxt("test_x")
+train_x=[[pixel/255 for pixel in pic] for pic in train_x]
+architectures=[[pic_size,100,nclasses]]
+#architectures=[[pic_size,1,nclasses]]
+#epocs=[300]
+#epocs=[1,3]
+learning_rates=[0.01]
+weight_init_boundries=[0.08]
+tst_name="all"
+
+# #success
+# pic_size=784
+# nclasses=10
+# train_x = np.loadtxt("train_x_top_100")
+# train_y = np.loadtxt("train_y_top_100")
+# test_x = np.loadtxt("train_x_top_10")
+# architectures=[[pic_size,100,nclasses]]
+# epocs=[300]
+# learning_rates=[0.05]
+# weight_init_boundries=[0.5]
+# tst_name="top_100"
+
+# success 
+#pic_size=2
+#nclasses=2
+#test_x=[[0,0],
+#         [0,1],
+#         [1,0],
+#         [1,1]]
+#train_x=test_x*10
+#train_y=[0,1,1,0]*10
+#architectures=[[pic_size,50,nclasses]]
+#epocs=[300]
+#learning_rates=[0.05]
+#weight_init_boundries=[0.5]
+#tst_name="xor"
+
+# success:
+# nclasses=4
+# pic_size=2
+# train_x=[[0,0],
+#          [0,1],
+#          [1,0],
+#          [1,1]]
+# train_y=[0,1,2,3]
+# architectures=[[pic_size,4,nclasses]]
+# tst_name="identity"
+
+# success:
+# nclasses=2
+# pic_size=2
+# train_x=[[0,1],
+#          [1,0]]
+# train_y=[0,1]
+# architectures=[[pic_size,2,nclasses]]
+# tst_name="identity"
+
+## success:
+#nclasses=4
+#pic_size=2
+#train_x=[[0,1],
+#         [1,0]]
+#train_y=[0,1]
+#architectures=[[pic_size,3, nclasses]]
+#tst_name="identity"
+
+train_x=[np.array(X) for X in train_x]
+validation_ratio=.2
+Y=dict([(y,[ 1 if i==y else 0 for i in range(nclasses)]) for y in range(nclasses) ])
+logging.basicConfig(filename="/home/nadav/data/nn.log",level=logging.DEBUG)
+
+def init_model(params):
+    layer_sizes, weight_init_boundry = params
+    return [ np.matrix([[rnd.uniform(-weight_init_boundry,weight_init_boundry) for i in range(layer_sizes[l])] for j in range(layer_sizes[l+1])]) for l in range(len(layer_sizes)-1) ], \
+           [ np.matrix([rnd.uniform(-weight_init_boundry,weight_init_boundry) for j in range(layer_sizes[l+1])]) for l in range(len(layer_sizes)-1) ] #B
 class LossNegLogLikelihood:
     def __call__(self, V, y):
-        return -log(V[int(y)])
+        return -log(np.squeeze(np.asarray(V))[int(y)])
     def derivative_z(self, out, Y):
         return out-Y
 loss=LossNegLogLikelihood()
@@ -57,94 +118,87 @@ def split_to_valid(train_x,train_y):
     data_set=zip(train_x, train_y)
     rnd.shuffle(data_set)
     train_size=len(data_set)-int(validation_ratio*len(data_set))
-    return data_set[:train_size],data_set[train_size:]
-def fw_prop(W,X):
-    out_prev = X
-    # print("fw prop layer 0: {}".format(X))
-    out=[out_prev]
-    for i in range(len(W)):
-        x_in = np.dot(W[i],np.append(out_prev,1.))
-        out_prev=activation[i+1](x_in.reshape(x_in.shape[1],-1))
-        out.append(np.array(out_prev))
-        # print("fw prop layer {}: {}".format(i+1,out_prev))
-    return out
-def bk_prop(W,X,Y,nlayers,learning_rate):
-    out=fw_prop(W,X)
-    err=loss.derivative_z(out[-1], Y)
-    loss_derivative_in = err
-    for l in list(reversed(range(nlayers)))[:-1]:
-        out_prev=np.append(out[l-1],1.)
-        loss_derivative_W_l = np.outer(loss_derivative_in, out_prev) # calculate a matrix of the same shape as W[l] used to update it
-        err=np.squeeze(np.asarray(np.dot(np.transpose(W[l-1]), loss_derivative_in))) # pass the delta of layer l neurons down to layer l-1 neurons
-        W[l-1]-=learning_rate*loss_derivative_W_l # update layer l-1 to layer l weights
-        if l>1:
-            loss_derivative_in = activation[l-1].derivative(out_prev)[:-1]*err[:-1] # G_l = loss_derivative_z
-def validate(W,valid):
+    return data_set[:train_size],data_set[:train_size]
+sigmoid = ActivationSigmoid()
+def fprop(W,B,X):
+    W1=W[0]
+    b1=B[0]
+    W2=W[1]
+    b2=B[1]
+    x=X
+    z1 = np.dot(W1, x) + b1
+    h1 = sigmoid(z1)
+    z2 = np.dot(W2, h1.T).reshape(-1) + b2
+    h2 = sigmoid(z2)
+    return [h1,h2]
+def bprop(W,B,X,Y,learning_rate):
+    out_list=fprop(W,B,X)
+    h2 = out_list[1]
+    h1 = out_list[0]
+    y=np.matrix([Y])
+    dz2 = (h2 - y)
+    dW2 = np.outer(dz2, h1.T)
+    db2 = dz2  # dL/dz2 * dz2/db2
+    dz1 = np.array(np.dot(W[1].T, (h2 - y).T).reshape(-1).tolist()) * np.array(h1.tolist()) * (1. - np.array(h1.tolist()))
+    dW1 = np.outer(dz1, X.T)
+    db1 = dz1
+    W[1] = W[1] - learning_rate*dW2
+    B[1] = B[1] - learning_rate*db2
+    W[0] = W[0] - learning_rate*dW1
+    B[0] = B[0] - learning_rate*db1
+def validate(W,B,valid):
     sum_loss= 0.0
     correct=0.0
     for X, y in valid:
-        out = fw_prop(W,X)
+        out = fprop(W,B,X)
         sum_loss += loss(out[-1],y)
         if out[-1].argmax() == y:
             correct += 1
     return sum_loss/ len(valid),correct/ len(valid)
-def train(W,train_x,train_y,params):
-    starting_epoc, ending_epoc, learning_rate, layer_sizes, batch_size, weight_init_boundry, avg_loss_list, avg_acc_list = params
+def train(W,B,train_x,train_y,learning_rate,starting_epoc,ending_epoc,avg_loss_list,avg_acc_list):
     train,valid=split_to_valid(train_x,train_y)
     for e in range(starting_epoc,ending_epoc):
+        logging.debug("starting epoc {}".format(e))
         rnd.shuffle(train)
+        s=time.time()
         for X,y in train:
-            bk_prop(W,X,Y[y],len(layer_sizes),learning_rate)
-        avg_loss,acc=validate(W, valid)
+            bprop(W,B,X,Y[y],learning_rate)
+        duration=time.time()-s
+        avg_loss,acc=validate(W, B, valid)
+        logging.debug("epoc {} avg_loss {} acc {} duration {}".format(e,avg_loss,acc,duration))
         avg_loss_list.append(avg_loss)
         avg_acc_list.append(acc)
-        logging.error("epoc {} avg loss {} accuracy {}".format(e,avg_loss,acc))
-    epocs_list=list(range(ending_epoc))
+    epocs_list = list(range(epocs))
     plt.plot(epocs_list,avg_loss_list,'bs',epocs_list,avg_acc_list,'rs')
     plt.xlabel("epocs")
-    plt.savefig("/home/nadav/data/perf.e_{}.lr_{}.hs_{}.bs_{}.w_{}.png".format(ending_epoc,learning_rate,layer_sizes[1],batch_size,weight_init_boundry))
+    plt.savefig("/home/nadav/perf.tst_{}.e_{}.lr_{}.hs_{}.w_{}.png".format(tst_name,epocs,learning_rate,architectures[0][1],weight_init_boundries[0]))
     plt.clf()
-    return W, avg_loss_list, avg_acc_list
-def test_and_write(model,test_x,params):
-    '''
-    :param model: a learned weight matrices. the 1st layer W matrix width=|X|=pic_size & high=hidden_layer_size
-                  the 2nd layer matrix width=hidden_layer_size & high=nclasses
-    :param test_x:
-    :return:
-    '''
-    starting_epoc, epoc, learning_rate, layer_sizes, batch_size, weight_init_boundry, avg_loss_list, avg_acc_list = params
-    with open("/home/nadav/data/test.e_{}.lr_{}.hs_{}.bs_{}.w_{}".format(epoc,learning_rate,layer_sizes[1],batch_size,weight_init_boundry), 'w') as f:
+# def train(W,train_x,train_y,params):
+#     epocs_list=list(range(ending_epoc))
+def test(W,B,test_x):
+    c=0.0
+#    with open("/home/nadav/data/test.tst_{}.e_{}.lr_{}.hs_{}.w_{}".format(tst_name,epocs[0],learning_rates[0],architectures[0][1],weight_init_boundries[0]), 'w') as f:
+    with open("/home/nadav/data/test.pred", 'w') as f:
         for X in test_x:
-            f.write("{}\n".format(fw_prop(model,X)[-1].argmax()))
-#train_x = np.loadtxt("train_x")
-#train_y = np.loadtxt("train_y")
-#test_x = np.loadtxt("test_x")
-# train_x = np.loadtxt("train_x_top_10")
-# train_y = np.loadtxt("train_y_top_10")
-# test_x = np.loadtxt("test_x_top_10")
-
+            p=np.squeeze(np.asarray(fprop(W, B, X)[-1]))
+            print("p {} y_hat {}".format(p,p.argmax()))
+            f.write("{}\n".format(p.argmax()))
 #with open(r"data.txt", "wb") as f:
 #    f.write(pickle.dumps((train_x, train_y, test_x)))
-#sys.exit(0)
-data = open(r"/home/nadav/data/data.txt", "rb").read()
-train_x,train_y,test_x = pickle.loads(data)
-train_x=[[pixel/255 for pixel in pic] for pic in train_x]
 
 for weight_init_boundry in weight_init_boundries:
-    for layer_sizes in architectures:
-        for learning_rate in learning_rates:
-            params=layer_sizes, weight_init_boundry
-            W = init_model(params)
-            avg_loss_list = []
-            avg_acc_list = []
-            starting_epoc=0
-            for ending_epoc in epocs:
-                params = starting_epoc, ending_epoc, learning_rate, layer_sizes, batch_size, weight_init_boundry, avg_loss_list, avg_acc_list
-                logging.error("start training with params: e_{}.lr_{}.hs_{}.bs_{}.w_{}".format(ending_epoc,learning_rate,layer_sizes[1],batch_size,weight_init_boundry))
-                W, avg_loss_list, avg_acc_list =train(W,train_x,train_y,params)
-                starting_epoc = ending_epoc
-                with open(r"/home/nadav/data/W.e_{}.lr_{}.hs_{}.bs_{}.w_{}".format(ending_epoc,learning_rate,layer_sizes[1],batch_size,weight_init_boundry),"wb") as f:
-                    f.write(pickle.dumps((W,avg_loss_list, avg_acc_list)))
-                #pW=open(r"/home/nadav/data/W.e_{}.lr_{}.hs_{}.bs_{}.w_{}".format(epocs,learning_rate,layer_sizes[1],batch_size,weight_init_boundry)).read()
-                #W,avg_loss_list, avg_acc_list=pickle.loads(pW)
-                test_and_write(W,test_x,params)
+     for architecture in architectures:
+         for learning_rate in learning_rates:
+             W,B=init_model((architecture, weight_init_boundry))
+             avg_loss_list = []
+             avg_acc_list = []
+             starting_epoc=0
+             for ending_epoc in epocs:
+                 logging.error("start training with params: tst_{}.e_{}.lr_{}.hs_{}.w_{}".format(tst_name,ending_epoc,learning_rate,architecture[1],weight_init_boundry))
+                 avg_loss_list, avg_acc_list = train(W,B,train_x,train_y,learning_rate,starting_epoc,ending_epoc,avg_loss_list,avg_acc_list)
+                 starting_epoc = ending_epoc
+                 with open(r"/home/nadav/data/W.e_{}.lr_{}.hs_{}.w_{}".format(ending_epoc,learning_rate,architecture[1],weight_init_boundry),"wb") as f:
+                     f.write(pickle.dumps((W,avg_loss_list, avg_acc_list)))
+#                 #pW=open(r"/home/nadav/data/W.e_{}.lr_{}.hs_{}.w_{}".format(ending_epoc,learning_rate,architecture[1],weight_init_boundry)).read()
+#                 #W,avg_loss_list, avg_acc_list=pickle.loads(pW)
+                 test(W,B,test_x)
