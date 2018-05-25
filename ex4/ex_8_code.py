64f7dc301bca0afc78e8fc64d48a1d605e9f2c0c
nadav yakar <nadavyakar@gmail.com>
2018-05-25 12:42:16 +0300

    ex4 new unfinished

--- ex4/ex_8_code.py
+++ ex4/ex_8_code.py
@@ -4,6 +4,11 @@ from torchvision import datasets, transforms
 from torch import nn
 
 lr=0.001
+nfc0=100
+nfc1=50
+nclasses=10
+nfc2=nclasses
+image_size=28*28
 
 transforms = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))])
 train_loader = torch.utils.data.DataLoader(datasets.FashionMNIST('./data', train=True, download=True, transform=transforms), batch_size=64, shuffle=True)
@@ -13,22 +18,20 @@ class FirstNet(nn.Module):
     def __init__(self,image_size):
         super(FirstNet, self).__init__()
         self.image_size = image_size
-        self.fc0 = nn.Linear(image_size, 1000)
-        self.fc1 = nn.Linear(1000, 50)
-        self.fc2 = nn.Linear(50, 10)
+        self.fc0 = nn.Linear(image_size, nfc0)
+        self.fc1 = nn.Linear(nfc0, nfc1)
+        self.fc2 = nn.Linear(nfc1, nfc2)
     def forward(self, x):
         x = x.view(-1, self.image_size)
         x = F.relu(self.fc0(x))
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         return F.log_softmax(x)
-model = FirstNet(image_size=28*28)
 
-optimizer = optim.SGD(model.parameters(), lr=lr)
 # optimizer = optim.Adam(model.parameters(), lr=lr)
 # optimizer = optim.AdaDelta(model.parameters(), lr=lr)
 # optimizer = optim.RMSprop(model.parameters(), lr=lr)
-def train(epoch, model):
+def train(model,optimizer):
     model.train()
     for batch_idx, (data, labels) in enumerate(train_loader):
         optimizer.zero_grad()
@@ -36,8 +39,8 @@ def train(epoch, model):
         loss = F.nll_loss(output, labels)
         loss.backward()
         optimizer.step()
-
-def test():
+optimizer
+def test(model,):
     model.eval()
     test_loss = 0
     correct = 0
@@ -48,9 +51,10 @@ def test():
         correct += pred.eq(target.data.view_as(pred)).cpu().sum()
         test_loss /= len(test_loader.dataset)
         print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
-        test_loss, correct, len(test_loader.dataset),
-        100. * correct / len(test_loader.dataset)))
+            test_loss, correct, len(test_loader.dataset), 100. * correct / len(test_loader.dataset)))
 
-for epoch in range(1, 10 + 1):
-    train(epoch)
-    test()
+optimizer = optim.SGD(model.parameters(), lr=lr)
+for model in [FirstNet(image_size=image_size)]:
+    for epoch in range(10):
+        train(model,optimizer)
+        test(model,optimizer)
