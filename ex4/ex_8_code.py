83052c30914b7c2323768c1b37dab6f8fbaa1b5e
nadav yakar <nadavyakar@gmail.com>
2018-05-26 14:31:36 +0300

    ex4 final

--- ex4/ex_8_code.py
+++ ex4/ex_8_code.py
@@ -1,6 +1,7 @@
 import numpy as np
 import torch
-from torchvision import datasets, transforms
+from torchvision import datasets
+import torchvision.transforms as transforms_
 from torch import nn, optim
 import torch.nn.functional as F
 import matplotlib.pyplot as plt
@@ -63,96 +64,168 @@ def train(train_loader,model,optimizer):
         optimizer.step()
     return loss_sum /len(train_loader.dataset), 100. * correct / len(train_loader.dataset)
 
-def test_and_validate(dataset_loader, model, ndataset=None):
+def test_and_validate(dataset_loader, model, ndataset=None, return_pred=False):
     model.eval()
     loss = 0.
     correct = 0.
     if ndataset is None:
         ndataset = len(dataset_loader.dataset)
+    pred_list=[]
     for data, target in dataset_loader:
         output = model(data)
         loss += F.nll_loss(output, target, size_average=False).item()
         pred = output.data.max(1, keepdim=True)[1]
+        if return_pred:
+            pred_list+=[x.item() for x in pred]
         correct += pred.eq(target.data.view_as(pred)).cpu().sum()
-    return loss/ ndataset, 100. * correct / ndataset
+    return loss/ ndataset, 100. * correct / ndataset, pred_list
 
-# lr=0.01
 nfc0=100
 nfc1=50
 nclasses=10
 nfc2=nclasses
 image_size=28*28
 nepocs=10
-# batch_size=64
 valid_ratio=0.2
 
-for lr in [0.001,0.01, 0.05]:
-    print('learning rate'.format(lr))
-    for batch_size in [pow(2,x) for x in [0,3,5,6]]:
-        print('batch size'.format(batch_size))
-        transforms = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))])
-        train_dataset=datasets.FashionMNIST('./data', train=True, download=True, transform=transforms)
+experiments=[]
+# # experiment with models
+# # baseline
+# lr=0.01
+# batch_size=64
+# model=(FirstNet(image_size,nfc0,nfc1,nfc2),1)
+# optimizer=(optim.SGD(model[0].parameters(), lr=lr),"sgd")
+# experiments.append((lr,batch_size,model,optimizer))
+#
+# lr=0.01
+# batch_size=64
+# model=(SecondNet(image_size,nfc0,nfc1,nfc2),2)
+# optimizer=(optim.SGD(model[0].parameters(), lr=lr),"sgd")
+# experiments.append((lr,batch_size,model,optimizer))
+#
+# lr=0.01
+# batch_size=64
+# model=(ThirdNet(image_size,nfc0,nfc1,nfc2),3)
+# optimizer=(optim.SGD(model[0].parameters(), lr=lr),"sgd")
+# experiments.append((lr,batch_size,model,optimizer))
+#
+# # experiment with data
+# remote:
+# lr=0.01
+# batch_size=64
+# model=(FirstNet(image_size,nfc0,nfc1,nfc2),1)
+# optimizer=(optim.SGD(model[0].parameters(), lr=lr),"sgd")
+# experiments.append((lr,batch_size,model,optimizer))
+#
+# # experiment with learning rates
+# lr=0.001
+# batch_size=64
+# model=(FirstNet(image_size,nfc0,nfc1,nfc2),1)
+# optimizer=(optim.SGD(model[0].parameters(), lr=lr),"sgd")
+# experiments.append((lr,batch_size,model,optimizer))
+#
+# lr=0.05
+# batch_size=64
+# model=(FirstNet(image_size,nfc0,nfc1,nfc2),1)
+# optimizer=(optim.SGD(model[0].parameters(), lr=lr),"sgd")
+# experiments.append((lr,batch_size,model,optimizer))
+#
+# # experiment with batch sizes
+# lr=0.01
+# batch_size=1
+# model=(FirstNet(image_size,nfc0,nfc1,nfc2),1)
+# optimizer=(optim.SGD(model[0].parameters(), lr=lr),"sgd")
+# experiments.append((lr,batch_size,model,optimizer))
+#
+# lr=0.01
+# batch_size=8
+# model=(FirstNet(image_size,nfc0,nfc1,nfc2),1)
+# optimizer=(optim.SGD(model[0].parameters(), lr=lr),"sgd")
+# experiments.append((lr,batch_size,model,optimizer))
+#
+# lr=0.01
+# batch_size=32
+# model=(FirstNet(image_size,nfc0,nfc1,nfc2),1)
+# optimizer=(optim.SGD(model[0].parameters(), lr=lr),"sgd")
+# experiments.append((lr,batch_size,model,optimizer))
+#
+# # experiment with optimizers
+# lr=0.01
+# batch_size=64
+# model=(FirstNet(image_size,nfc0,nfc1,nfc2),1)
+# optimizer=(optim.Adam(model[0].parameters(), lr=lr), "adam")
+# experiments.append((lr,batch_size,model,optimizer))
+#
+# lr=0.01
+# batch_size=64
+# model=(FirstNet(image_size,nfc0,nfc1,nfc2),1)
+# optimizer=(optim.RMSprop(model[0].parameters(), lr=lr),"rmsprop")
+# experiments.append((lr,batch_size,model,optimizer))
+#
+# chosen setup
+lr=0.05
+batch_size=32
+model=(ThirdNet(image_size,nfc0,nfc1,nfc2),3)
+optimizer=(optim.SGD(model[0].parameters(), lr=lr),"sgd")
+experiments.append((lr,batch_size,model,optimizer))
 
-        ntrain = len(train_dataset)
-        indices = list(range(ntrain))
-        nvalid = int(valid_ratio*len(train_dataset))
+transforms = transforms_.Compose([transforms_.ToTensor(), transforms_.Normalize((0.1307,), (0.3081,))])
+train_dataset = datasets.FashionMNIST('./data', train=True, download=True, transform=transforms)
 
-        validation_idx = np.random.choice(indices, size=nvalid, replace=False)
-        train_idx = list(set(indices) - set(validation_idx))
+ntrain = len(train_dataset)
+indices = list(range(ntrain))
+nvalid = int(valid_ratio * len(train_dataset))
+ntrain-=nvalid
 
-        train_sampler = SubsetRandomSampler(train_idx)
-        validation_sampler = SubsetRandomSampler(validation_idx)
+validation_idx = np.random.choice(indices, size=nvalid, replace=False)
+train_idx = list(set(indices) - set(validation_idx))
 
-        train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=batch_size, sampler=train_sampler)
-        validation_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=batch_size, sampler=validation_sampler)
+train_sampler = SubsetRandomSampler(train_idx)
+validation_sampler = SubsetRandomSampler(validation_idx)
 
-        test_loader = torch.utils.data.DataLoader(datasets.FashionMNIST('./data', train=False, transform=transforms), batch_size=batch_size, shuffle=True)
+train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=batch_size, sampler=train_sampler)
+validation_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=batch_size,
+                                                sampler=validation_sampler)
 
-        i=0
-        for model in [FirstNet(image_size,nfc0,nfc1,nfc2),SecondNet(image_size,nfc0,nfc1,nfc2),ThirdNet(image_size,nfc0,nfc1,nfc2)]:
-            i+=1
-            print("\nmodel number {}".format(i))
-            for optimizer, optimizer_name in [ (optim.SGD(model.parameters(), lr=lr),"sgd"),
-                                               (optim.Adam(model.parameters(), lr=lr), "adam"),
-                                               (optim.RMSprop(model.parameters(), lr=lr),"rmsprop") ]:
-                print("optimizer {}".format(optimizer_name))
-                # optimizer = optim.SGD(model.parameters(), lr=lr)
-                # optimizer = optim.Adam(model.parameters(), lr=lr)
-                # optimizer = optim.AdaDelta(model.parameters(), lr=lr)
-                # optimizer = optim.RMSprop(model.parameters(), lr=lr)
-                epocs_list = list(range(nepocs))
-                train_loss=0.0
-                avg_train_loss_list=[]
-                train_acc=0.0
-                train_acc_list=[]
-                valid_loss=0.0
-                avg_valid_loss_list=[]
-                valid_acc = 0.0
-                valid_acc_list=[]
+test_loader = torch.utils.data.DataLoader(datasets.FashionMNIST('./data', train=False, transform=transforms),
+                                          batch_size=batch_size, shuffle=False)
 
-                for e in epocs_list:
-                    print("epoc {}".format(e))
-                    train_loss, train_acc = train(train_loader, model,optimizer)
+for lr,batch_size, (model,i), (optimizer, optimizer_name) in experiments:
+    print("model number: {}, optimizer: {}, learning rate: {}, batch size: {}".format(i,optimizer_name,lr,batch_size))
+    epocs_list = list(range(nepocs))
+    train_loss=0.0
+    avg_train_loss_list=[]
+    train_acc=0.0
+    train_acc_list=[]
+    valid_loss=0.0
+    avg_valid_loss_list=[]
+    valid_acc = 0.0
+    valid_acc_list=[]
+    for e in epocs_list:
+        print("epoc: {}".format(e))
+        train_loss, train_acc = train(train_loader, model,optimizer)
 
-                    train_loss, train_acc = test_and_validate(train_loader, model, ntrain)
-                    avg_train_loss_list.append(train_loss)
-                    train_acc_list.append(train_acc)
+        train_loss, train_acc, dummy_list = test_and_validate(train_loader, model, ntrain)
+        avg_train_loss_list.append(train_loss)
+        train_acc_list.append(train_acc)
 
-                    valid_loss, valid_acc = test_and_validate(validation_loader, model, nvalid)
-                    avg_valid_loss_list.append(valid_loss)
-                    valid_acc_list.append(valid_acc)
-                test_avg_loss, test_acc = test_and_validate(test_loader, model)
-                print('Training set: Average loss: {:.4f}, Accuracy: {:.0f}%'.format(train_loss, train_acc))
-                print('Validation set: Average loss: {:.4f}, Accuracy: {:.0f}%'.format(valid_loss, valid_acc))
-                print('Test set: Average loss: {:.4f}, Accuracy: {:.0f}%'.format(test_avg_loss, test_acc))
+        valid_loss, valid_acc, dummy_list = test_and_validate(validation_loader, model, nvalid)
+        avg_valid_loss_list.append(valid_loss)
+        valid_acc_list.append(valid_acc)
+    test_avg_loss, test_acc, pred_list = test_and_validate(test_loader, model, None, True)
+    print('Training set: Average loss: {:.4f}, Accuracy: {:.0f}%'.format(train_loss, train_acc))
+    print('Validation set: Average loss: {:.4f}, Accuracy: {:.0f}%'.format(valid_loss, valid_acc))
+    print('Test set: Average loss: {:.4f}, Accuracy: {:.0f}%'.format(test_avg_loss, test_acc))
 
-                # plt.plot(epocs_list,avg_train_loss_list,color='blue',linewidth=1, markersize=1)
-                plt.plot(epocs_list,avg_train_loss_list,'red',epocs_list,avg_valid_loss_list,'green',linewidth=1, markersize=1)
-                plt.xlabel("epocs")
-                plt.savefig("loss_model_{}.optimizer_{}.batch_{}.lr_{}.png".format(i,optimizer_name,batch_size,lr))
-                plt.clf()
-                # plt.plot(epocs_list,train_acc_list,color='blue',linewidth=1, markersize=1)
-                plt.plot(epocs_list,train_acc_list,'red',epocs_list,valid_acc_list,'green',linewidth=1, markersize=1)
-                plt.xlabel("epocs")
-                plt.savefig("accuracy_model_{}.optimizer_{}.batch_{}.lr_{}.png".format(i,optimizer_name,batch_size,lr))
-                plt.clf()
+    plt.plot(epocs_list,avg_train_loss_list,'red',epocs_list,avg_valid_loss_list,'green',linewidth=1, markersize=1)
+    plt.xlabel("epocs")
+    plt.savefig("loss.model_{}.optimizer_{}.batch_{}.lr_{}.png".format(i,optimizer_name,batch_size,lr))
+    plt.clf()
+    plt.plot(epocs_list,train_acc_list,'red',epocs_list,valid_acc_list,'green',linewidth=1, markersize=1)
+    plt.xlabel("epocs")
+    plt.savefig("accuracy.model_{}.optimizer_{}.batch_{}.lr_{}.png".format(i,optimizer_name,batch_size,lr))
+    plt.clf()
+
+    with open("test.pred",'w') as f:
+        for pred in pred_list:
+            f.write("{}\n".format(pred))
